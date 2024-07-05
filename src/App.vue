abc59e5ed5644e8d282c30f4e38c575b830f096b
samuelecozzubbo <samuele.cozzubbo@gmail.com>
2024-07-05 17:42:22 +0200

    Build:implementing AppShop component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,29 @@
 <script>
 import AppHeader from './components/AppHeader.vue'
+import AppShop from './components/AppShop.vue'
 
 export default {
   name: 'App',
   components: {
     AppHeader,
+    AppShop,
   }
 }
 </script>
 
 <template>
   <AppHeader />
+  <main>
+    Parte centrale
+  </main>
+  <AppShop/>
 </template>
 
-<style scoped>
-
+<style lang="scss">
+main{
+    background-color: black;
+    width: 100%;
+    min-height: 100px;
+    color: white;
+}
 </style>
---
+++ src/components/AppShop.vue
@@ -0,0 +1,44 @@
+<script>
+export default {
+    name: 'AppShop',
+    data() {
+        return {
+            links: [
+                {
+                    text: "DIGITAL COMICS",
+                    imgUrl: "../assets/img/buy-comics-digital-comics.png",
+                    current: false,
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<template>
+    <section>
+        <div class="container">
+            <ul>
+                <li v-for="(link,index) in links" :key="index">
+                    <img :src="link.imgUrl" alt="img">
+                </li>
+            </ul>
+
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/general.scss' as *;
+@use '../styles/partials/mixings' as *;
+
+section{
+    background-color: #0282F9;
+    max-width: 100%;
+    min-height: 50px;
+}
+
+.container{
+    @include center(space-between)
+}
+</style>
