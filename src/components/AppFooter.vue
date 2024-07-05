c4e5131752af4a161dc168730cfee6647e0b3d5c
samuelecozzubbo <samuele.cozzubbo@gmail.com>
2024-07-05 18:50:49 +0200

    Build: implementing footer-links

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -6,6 +6,102 @@
 
 <template>
 
+    <footer>
+        <section class="footer-links">
+            <div class="container">
+                <!-- DC Comics Links -->
+                <div class="column">
+                    <h4>DC COMICS</h4>
+                    <ul>
+                    <li><a href="#">Characters</a></li>
+                    <li><a href="#">Comics</a></li>
+                    <li><a href="#">Movies</a></li>
+                    <li><a href="#">TV</a></li>
+                    <li><a href="#">Games</a></li>
+                    <li><a href="#">Videos</a></li>
+                    <li><a href="#">News</a></li>
+                    </ul>
+                    <h4>SHOP</h4>
+                    <ul>
+                    <li><a href="#">Shop DC</a></li>
+                    <li><a href="#">Shop DC Collectibles</a></li>
+                    </ul>
+                </div>
+                <div class="column">
+                    <h4>DC</h4>
+                    <ul>
+                    <li><a href="#">Terms Of Use</a></li>
+                    <li><a href="#">Privacy policy (New)</a></li>
+                    <li><a href="#">Ad Choices</a></li>
+                    <li><a href="#">Advertising</a></li>
+                    <li><a href="#">Jobs</a></li>
+                    <li><a href="#">Subscriptions</a></li>
+                    <li><a href="#">Talent Workshops</a></li>
+                    <li><a href="#">CPSC Certificates</a></li>
+                    <li><a href="#">Ratings</a></li>
+                    <li><a href="#">Shop Help</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                    </ul>
+                </div>
+                <div class="column">
+                    <h4>SITES</h4>
+                    <ul>
+                    <li><a href="#">DC</a></li>
+                    <li><a href="#">MAD Magazine</a></li>
+                    <li><a href="#">DC Kids</a></li>
+                    <li><a href="#">DC Universe</a></li>
+                    <li><a href="#">DC Power Visa</a></li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+    </footer>
+
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use '../styles/general.scss' as *;
+@use '../styles/partials/mixings' as *;
+
+.footer-links{
+    padding: 30px 0;
+    background-image: url('/img/footer-bg.jpg'), url('/img/dc-logo-bg.png');
+    background-size: cover,30%;
+    background-repeat: no-repeat;
+
+    .container{
+        display: flex;
+    }
+
+    .column {
+        margin-right: 20px;
+
+        h4 {
+        font-size: 14px;
+        margin-bottom: 10px;
+        color: white;
+        }
+
+        ul {
+        list-style: none;
+        padding: 0;
+
+        li {
+            margin-bottom: 5px;
+
+            a {
+            color: #999;
+            text-decoration: none;
+            font-size: 12px;
+
+            &:hover {
+                color: #fff;
+            }
+            }
+        }
+        }
+    }
+}
+
+
+</style>
