ed099aadb2198ce5721d2ba6d06cc9c3e2546b67
samuelecozzubbo <samuele.cozzubbo@gmail.com>
2024-07-05 16:39:32 +0200

    Build: create components and implement AppHeader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,104 @@
+<script>
+export default {
+    name: 'AppHeader',
+    data() {
+        return {
+            links: [
+                {
+                    text: "CHARACTERS",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "COMICS",
+                    url: "#",
+                    current: true,
+                },
+                {
+                    text: "MOVIES",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "TV",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "GAMES",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "COLLECTIBLES",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "VIDEOS",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "FANS",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "NEWS",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "SHOP",
+                    url: "#",
+                    current: false,
+                },
+
+            ]
+        }
+    }
+}
+</script>
+
+<template>
+    <header>
+        <div class="container">
+            <a href="/" class="brand">
+            <img src="../assets/img/dc-logo.png"
+                alt="logo">
+        </a>
+
+        <!-- Navigazione -->
+        <nav>
+            <ul>
+                <li v-for="(link, index) in links" :key="index">
+                    <a :class="{ active: link.current }" :href="link.url">{{ link.text }}</a>
+                </li>
+            </ul>
+        </nav>
+        </div>
+        
+    </header>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/general.scss' as *;
+@use '../styles/partials/mixings' as *;
+
+.container,ul{
+    @include center(space-between);
+}
+
+ul{
+    gap: 12px;
+    list-style: none;
+
+    a{
+        text-decoration: none;
+        color: #55595C;
+    }
+}
+
+
+</style>
